eddb2b17154c535dc42eaa0d762ae19d10b9d5d5
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2021-08-03 14:56:20 +0300

    feat(notification): better styles

--- src/App.vue
+++ src/App.vue
@@ -48,12 +48,22 @@
         <v-row dense justify="center">
           <v-col cols="6">
             <v-alert
-              border="left"
-              icon="mdi-account"
-              variant="outlined"
-              prominent
-              color="error"
-            >Attention!</v-alert>
+              color="success"
+              closable
+            >
+              Notification example<br />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </v-alert>
+          </v-col>
+          <v-col cols="6">
+            <v-alert
+              variant="contained"
+              color="success"
+              closable
+            >
+              Notification example<br />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </v-alert>
           </v-col>
         </v-row>
 
